7d59a63739a5ecf479fb846ae4ef37b645fca494
Ahmad Sohrabi <asohrabi@hotmail.com>
2020-02-01 20:52:00 +0330

    aa

---
+++ ChargeLearning/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Blazor.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChargeLearning
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
+            BlazorWebAssemblyHost.CreateDefaultBuilder()
+                .UseBlazorStartup<Startup>();
+    }
+}
